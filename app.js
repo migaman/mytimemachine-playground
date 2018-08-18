30ef4167658bc8654f4d064aafe54136c37f0432
migaman <mikee@bluewin.ch>
2018-08-18 21:57:24 +0200

    EJS Template

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var bodyparser = require('body-parser');
 var app = express();
 
 const EXAMPLE_ADDRESS = process.env.EXAMPLE_CONTRACT_ADDRESS || '0xca4b024f3f7279534ccb5dc4a528c46afa79eed3';
+//Needs feature Dyno Metadata (https://stackoverflow.com/questions/7917523/how-do-i-access-the-current-heroku-release-version-programmatically)
+const VERSION = process.env.HEROKU_RELEASE_VERSION;
 
 app.set('view engine', 'ejs');
 app.use(bodyparser.json());
@@ -15,7 +17,7 @@ app.use(express.static('./static'));
 app.use('/contracts', express.static('./build/contracts'));
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Example Page', address: EXAMPLE_ADDRESS });
+  res.render('index', { title: 'Example Page', address: EXAMPLE_ADDRESS, releaseversion: VERSION });
 });
 
 var port = process.env.PORT || 3000;
--- deployContracts.bat
+++ deployContracts.bat
@@ -1,10 +1,3 @@
-REM call truffle.cmd console
-
-REM  migrate --reset
-REM  VideoContract.deployed().then(function(instance){return instance.getBlockNumber();});
-REM  VideoContract.deployed().then(function(instance){return instance.addVideo(10, "test", 7);});
-
-REM  VideoContract.deployed().then(function(instance){return instance.getVideoId();});
-
-call truffle.cmd migrate --reset
+REM Local (Ganache)
+call truffle.cmd migrate --reset --network development
 pause
